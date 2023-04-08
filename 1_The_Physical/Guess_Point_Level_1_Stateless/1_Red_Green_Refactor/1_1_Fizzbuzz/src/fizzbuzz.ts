b676786bf2630342155259d03d40830cc3d5bf50
Wesley Finck <wesleyfinck@gmail.com>
2023-04-08 16:01:38 -0600

    [new + fix]: return Fizz for multiples of 3, change 6 to 7 when testing non multiples of 3 or 5

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,5 +1,6 @@
 export const fizzbuzz = (n: number) => {
     if (n < 1) throw "number is below 1";
     if (n > 100) throw "number is above 100";
+    if (n % 3 === 0) return "Fizz";
     return n.toString();
 };
--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -6,8 +6,8 @@ describe("fizzbuzz", () => {
         expect(typeof fizzbuzz(1)).toBe("string");
     });
 
-    it("returns a string version of the numbers 1, 2, 4, 6, non mulitples of 5 or 3", () => {
-        const nonMultiples = [1, 2, 4, 6];
+    it("returns a string version of the numbers 1, 2, 4, 7, non mulitples of 5 or 3", () => {
+        const nonMultiples = [1, 2, 4, 7];
         nonMultiples.map((nonMultiple) => fizzbuzz(nonMultiple)).forEach((nonMultipleString, index) => expect(nonMultipleString).toEqual(nonMultiples[index].toString()));
     })
 
@@ -17,5 +17,9 @@ describe("fizzbuzz", () => {
 
     it("throws an error when input is above 100", () => {
         expect(() => fizzbuzz(101)).toThrow()
+    })
+
+    it("returns 'Fizz' for 3, 6, 9, 12, multiples of 3", () => {
+        [3, 6, 9, 12].map((multiple) => fizzbuzz(multiple)).forEach((result) => expect(result).toEqual("Fizz"));
     })
 });
