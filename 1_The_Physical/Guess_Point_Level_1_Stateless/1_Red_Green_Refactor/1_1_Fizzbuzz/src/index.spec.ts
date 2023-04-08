af81b081e606ef80583b280bbe87b16e39f4e348
Wesley Finck <wesleyfinck@gmail.com>
2023-04-08 15:42:14 -0600

    [new]: fizzbuzz returns a string for 1

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,5 +1,8 @@
+import { fizzbuzz } from "./fizzbuzz";
 
 describe("fizzbuzz", () => {
 
-
+    it("returns a string for values between 1 and 100", () => {
+        expect(typeof fizzbuzz(1)).toBe("string");
+    });
 });
